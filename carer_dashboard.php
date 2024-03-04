8c0c4a9fc16471652777af5565c686acefbb9ec0
lj-2605 <148460950+lj-2605@users.noreply.github.com>
2024-03-04 15:01:36 +0000

    Add files via upload

---
+++ carer_dashboard.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Carer Dashboard</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="#"><img src="logo.jpg" alt="Helping Hands Logo"> Helping Hands</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="carer_dashboard.php">Dashboard</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="employee_timetable.php">Employee Timetable</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="personal_details.php">Personal Details</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="payroll_information.php">Payroll Information</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+  <h2>Welcome to Carer Dashboard</h2>
+  <!-- Include content for carer dashboard here -->
+</div>
+
+</body>
+</html>
